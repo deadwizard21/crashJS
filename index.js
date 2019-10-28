51ac0237a2bf7b01f22d453b49be149c506abb4a
deadwizard21 <deadwizard21@gmail.com>
2019-10-28 00:02:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,54 @@
+/*
 console.log ('hello world'); //statement, an expression to be carried out
 //testing
+let name = "mash";
+console.log(name);
+
+const interestRate = 0.3;
+//interestRate = 1.0;
+console.log(interestRate)
+
+
+let person = {
+    name: 'mosh',
+    age: 30
+
+}
+
+//dot notation
+//person.age = 40;
+
+//bracket notation
+person['name']= 'mary';
+console.log(person.name);
+
+
+let selectedColors = ['red','blue'];
+selectedColors[2] = 'green';
+console.log(selectedColors[0]);
+
+
+
+//function decleration
+//1 paramenter, name
+//nameis only meaning full inside the function
+function greet(name){
+    //body of the function
+    //here we add all the statemnts or logic
+    console.log('hello '+ name);
+};
+
+//the () to state that this is a statement
+greet('billy');
+
+function square(number){
+    return number * number;
+
+};
+
+let number = square(2);
+console.log(number)
+*/
+
+
+//  https://www.youtube.com/watch?v=PFmuCDHHpwk
